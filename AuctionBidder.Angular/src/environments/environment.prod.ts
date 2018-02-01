f125a92775519278cafd57327df8cd2b08673030
Travis Matthews <travis.matthews90@gmail.com>
2018-02-01 15:06:46 -0600

    updated prod environment bid connection string

--- AuctionBidder.Angular/src/environments/environment.prod.ts
+++ AuctionBidder.Angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   signalR: {
-    bidConnection: 'http://localhost:5000/bid'
+    bidConnection: 'https://auctionbidder-signalr.azurewebsites.net/bid'
   }
 };
